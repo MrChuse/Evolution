753e5a67d1cf8886ea94cb2a573c378abdfbe954
MrChuse <aleksha007@mail.ru>
2020-02-19 00:07:58 +0300

    Added documentation

--- brain/interpteterBrain.py
+++ brain/interpteterBrain.py
@@ -2,6 +2,34 @@ from baseBrain import BaseBrain
 import logging
 
 class InterpreterBrain(BaseBrain):
+    """
+    This class implements a decision-making machine with a turing-complete language
+
+    Sub Classes
+    -----------
+    ModuloInteger
+        A class to perform modulo arithmetics
+
+    Attributes
+    ----------
+    data : list of ints
+        The program to be interpreted (should not be changed)
+    commands : list of tuples (create a Command class?)
+        All possible actions of the environment
+    command_limit : int
+        Limits the number of commands executed before final action
+    pointer : ModuloInteger
+        Pointer of the current command
+    counter_limit : int
+        Counter of not final commands executed
+
+    Methods
+    -------
+    make_a_move(self, sensor_data):
+        calculates the next action and moves pointer further
+    mutate(self, prob, number_of_bytes=1):
+        changes number_of_bytes commands in the data with probability prob
+    """
 
     class ModuloInteger:
         def __new__(cls, value, modulo):
@@ -29,6 +57,16 @@ class InterpreterBrain(BaseBrain):
             return self.value > other.value
 
     def __init__(self, data, commands, command_limit):
+        """
+        Parameters
+        ----------
+        data : list of ints
+            The program to be interpreted (should not be changed)
+        commands : list of tuples (create a Command class?)
+            All possible actions of the environment
+        counter_limit : int
+            Counter of not final commands executed
+        """
         self.data = data
         self.commands = commands
         self.command_limit = command_limit
@@ -37,25 +75,42 @@ class InterpreterBrain(BaseBrain):
         self.counter_limit = 0
 
     def make_a_move(self, sensor_data):
+        """
+        Main method for the interpreter
+
+        Parameters
+        ----------
+        sensor_data : ???
+            All the data provided by the environment
+
+        Summary of the code:
+            READ the command from data[pointer]
+            IF it is final command THEN return that action and its arguments
+            ELSE execute that command (it shouldn't interact with the world,
+                 only with visible data or the pointer)
+            move the pointer to the next command
+
+        TODO: conditional and unconditional jumps, vision of the data
+        """
+
+        #execute commands until the limit is reached
         while self.counter_limit < self.command_limit:
-            #print(self.pointer,str(type(self.pointer)))
             current_command_id = self.data[self.pointer]
             if current_command_id >= len(self.commands):
-                self.pointer += 1
-                self.counter_limit += 1
+                self.pointer += 1      #if command is invalid, move the pointer,
+                self.counter_limit += 1  #update the counter, proceed to the next command
                 continue
-            if self.commands[current_command_id][1] is True:
+            if self.commands[current_command_id][1] is True: #if the command is final
                 from_data = self.pointer
-                #print(from_data, 'from_data')
-                self.pointer += (self.commands[current_command_id][0] + 1)
-                #print(self.data[from_data : self.pointer], from_data, self.pointer)
-                if from_data > self.pointer:
+                self.pointer += (self.commands[current_command_id][0] + 1) #move the pointer
+                if from_data > self.pointer: #check for the overlap
                     ret = self.data[from_data:] + self.data[:self.pointer]
                 else:
                     ret = self.data[from_data : self.pointer]
-                return ret
-
-        return -1
+                return ret #return action and its arguments
+            else: #TODO: jumps, vision
+                pass
+        return -1 #return -1 if no action was chosen
 
 def main():
     data = [0, 0, 0, 0, 0, 0, 0, 1]
--- brain/randomBrain.py
+++ brain/randomBrain.py
@@ -2,6 +2,19 @@ import random
 from baseBrain import BaseBrain
 
 class RandomBrain(BaseBrain):
+    """
+    The simplest (and probably the dumbest) brain possible
+
+    Attributes
+    ----------
+    number_of_moves : int
+        The number of possible actions an environment has
+
+    Methods
+    -------
+    make_a_move(self, sensor_data):
+        returns a random number from 0 to number_of_moves
+    """
 
     def __init__(self, number_of_moves):
         self.number_of_moves = number_of_moves
