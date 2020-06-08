bd7e7a45e37333889596fd015eef1169f88773ae
MrChuse <43815531+MrChuse@users.noreply.github.com>
2020-06-08 14:41:32 +0300

    Update baseCommands.py
    Added check ally, is occupied, is food present, compare amount of meat, compare amount of minerals, compare temperature to positive arg2, compare temperature to negative arg2

--- core/baseCommands.py
+++ core/baseCommands.py
@@ -1,18 +1,35 @@
-def move_pointer_check_ally(sensor_data, command_and_arguments):
-    return command_and_arguments[2] if brain.check_ally(other) else command_and_arguments[3]
+def ucj(sensor_data, com_arg): # UnConditional Jump
+    return com_arg[1]
+def cha(sensor_data, com_arg): # CHeck Ally
+    return com_arg[2] if sensor_data[0].agent.brain.check_ally(sensor_data[com_arg[1]].agent.brain) else com_arg[3]
+def occ(sensor_data, com_arg): # is OCCupied
+    return com_arg[2] if sensor_data[com_arg[1]].is_occupied() else com_arg[3]
+def ifd(sensor_data, com_arg): # Is FooD present
+    return com_arg[2] if sensor_data[com_arg[1]].is_food_here() else com_arg[3]
+def ame(sensor_data, com_arg): # Amount of MEat
+    return com_arg[3] if sensor_data[com_arg[1]].get_amount_of_meat() > com_arg[2] else com_arg[4]
+def ami(sensor_data, com_arg): # Amount of MInerals
+    return com_arg[3] if sensor_data[com_arg[1]].get_amount_of_minerals() > com_arg[2] else com_arg[4]
+def tmp(sensor_data, com_arg): # TeMPerature Positive
+    return com_arg[3] if sensor_data[com_arg[1]].get_temperature() > com_arg[2] else com_arg[4]
+def tmn(sensor_data, com_arg): # TeMPerature Negative
+    return com_arg[3] if sensor_data[com_arg[1]].get_temperature() > -com_arg[2] else com_arg[4]
 
-# is_occupied = self.is_occupied(target_pos)
-# is_food_here = self.field[target_pos[0]][target_pos[1]].is_food_here()
-# amount_of_meat = self.field[target_pos[0]][target_pos[1]].get_amount_of_meat()
-# amount_of_minerals = self.field[target_pos[0]][target_pos[1]].get_amount_of_minerals()
-# temperature = self.field[target_pos[0]][target_pos[1]].get_temperature()
-
-photosynthesis = (0, True)  # id = 0
-move = (2, True)  # id = 1
-eat = (3, True)  # id = 2
-give_birth_to = (3, True)  # id = 3
-share_energy = (3, True)  # id = 4
-unconditional_jump = (1, False, lambda sensor_data, command_and_arguments: command_and_arguments[1])
+photosynthesis         = (0, True)        # id = 0
+move                   = (2, True)        # id = 1
+eat                    = (3, True)        # id = 2
+give_birth_to          = (3, True)        # id = 3
+share_energy           = (3, True)        # id = 4
+unconditional_jump     = (1, False, ucj)  # id = 5
+check_ally             = (3, False, cha)  # id = 6
+is_occupied            = (3, False, occ)  # id = 7
+is_food_present        = (3, False, ifd)  # id = 8
+compare_amt_meat       = (4, False, ame)  # id = 9
+compare_amt_minerals   = (4, False, ami)  # id = 10
+compare_tmp_pos        = (4, False, tmp)  # id = 11
+compare_tmp_neg        = (4, False, tmn)  # id = 12
 
 check_ally = (5, False, move_pointer_check_ally)
-base_commands = [photosynthesis, move, eat, give_birth_to, share_energy, unconditional_jump]
+base_commands = [photosynthesis, move, eat, give_birth_to, share_energy, unconditional_jump,
+                 check_ally, is_occupied, is_food_present, compare_amt_meat, compare_amt_minerals,
+                 compare_tmp_pos, compare_tmp_neg]
