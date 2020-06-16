89ce1e1063769ee4fc4e0c8a080630f1df56a93a
MrChuse <aleksha007@mail.ru>
2020-06-16 12:41:22 +0300

    added getitem and len

--- core/statistics.py
+++ core/statistics.py
@@ -5,8 +5,13 @@ class Statistics:
     
     def __init__(self):
         self.dict = defaultdict(list)
+
+    def __getitem__(self, item):
+        return self.dict[item]
+
+    def __len__(self):
+        return len(next(iter(self.dict.values())))
     
     def add_tick(self, **tick):
-        for key, value in tick:
+        for key, value in tick.items():
             self.dict[key].append(value)
-   
